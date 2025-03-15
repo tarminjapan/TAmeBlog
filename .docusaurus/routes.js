1e026049bec581f5c759b924a906e746f5cb92a8
tarminjapan <t_amemiya_0124@outlook.jp>
2025-03-15 15:37:33 +0900

    feat: devcontainer を削除
    開発環境として devcontainer を使用する必要がなくなったため、関連ファイルを削除します。

---
+++ .docusaurus/routes.js
@@ -0,0 +1,209 @@
+import React from 'react';
+import ComponentCreator from '@docusaurus/ComponentCreator';
+
+export default [
+  {
+    path: '/TAmeBlog/__docusaurus/debug',
+    component: ComponentCreator('/TAmeBlog/__docusaurus/debug', 'c5d'),
+    exact: true
+  },
+  {
+    path: '/TAmeBlog/__docusaurus/debug/config',
+    component: ComponentCreator('/TAmeBlog/__docusaurus/debug/config', '86f'),
+    exact: true
+  },
+  {
+    path: '/TAmeBlog/__docusaurus/debug/content',
+    component: ComponentCreator('/TAmeBlog/__docusaurus/debug/content', '901'),
+    exact: true
+  },
+  {
+    path: '/TAmeBlog/__docusaurus/debug/globalData',
+    component: ComponentCreator('/TAmeBlog/__docusaurus/debug/globalData', '6f1'),
+    exact: true
+  },
+  {
+    path: '/TAmeBlog/__docusaurus/debug/metadata',
+    component: ComponentCreator('/TAmeBlog/__docusaurus/debug/metadata', '50b'),
+    exact: true
+  },
+  {
+    path: '/TAmeBlog/__docusaurus/debug/registry',
+    component: ComponentCreator('/TAmeBlog/__docusaurus/debug/registry', '640'),
+    exact: true
+  },
+  {
+    path: '/TAmeBlog/__docusaurus/debug/routes',
+    component: ComponentCreator('/TAmeBlog/__docusaurus/debug/routes', 'b2b'),
+    exact: true
+  },
+  {
+    path: '/TAmeBlog/blog/authors',
+    component: ComponentCreator('/TAmeBlog/blog/authors', '16e'),
+    exact: true
+  },
+  {
+    path: '/TAmeBlog/markdown-page',
+    component: ComponentCreator('/TAmeBlog/markdown-page', 'ebe'),
+    exact: true
+  },
+  {
+    path: '/TAmeBlog/docs',
+    component: ComponentCreator('/TAmeBlog/docs', '119'),
+    routes: [
+      {
+        path: '/TAmeBlog/docs',
+        component: ComponentCreator('/TAmeBlog/docs', 'd36'),
+        routes: [
+          {
+            path: '/TAmeBlog/docs',
+            component: ComponentCreator('/TAmeBlog/docs', 'd13'),
+            routes: [
+              {
+                path: '/TAmeBlog/docs/Atcoder Begginer Contest/200s/224/abc224c_01',
+                component: ComponentCreator('/TAmeBlog/docs/Atcoder Begginer Contest/200s/224/abc224c_01', '9e2'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/TAmeBlog/docs/Atcoder Begginer Contest/200s/225/abc225c_01',
+                component: ComponentCreator('/TAmeBlog/docs/Atcoder Begginer Contest/200s/225/abc225c_01', '1a2'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/TAmeBlog/docs/Atcoder Begginer Contest/200s/233/abc233d_01',
+                component: ComponentCreator('/TAmeBlog/docs/Atcoder Begginer Contest/200s/233/abc233d_01', '5b8'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/TAmeBlog/docs/Atcoder Begginer Contest/200s/234/abc234d_01',
+                component: ComponentCreator('/TAmeBlog/docs/Atcoder Begginer Contest/200s/234/abc234d_01', 'cb6'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/TAmeBlog/docs/Atcoder Begginer Contest/200s/239/abc239d_01',
+                component: ComponentCreator('/TAmeBlog/docs/Atcoder Begginer Contest/200s/239/abc239d_01', '9a7'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/TAmeBlog/docs/Atcoder Begginer Contest/200s/257/abc257d_01',
+                component: ComponentCreator('/TAmeBlog/docs/Atcoder Begginer Contest/200s/257/abc257d_01', '6e1'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/TAmeBlog/docs/Atcoder Begginer Contest/200s/272/abc272c_01',
+                component: ComponentCreator('/TAmeBlog/docs/Atcoder Begginer Contest/200s/272/abc272c_01', '984'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/TAmeBlog/docs/Atcoder Begginer Contest/200s/273/abc273e_01',
+                component: ComponentCreator('/TAmeBlog/docs/Atcoder Begginer Contest/200s/273/abc273e_01', 'c19'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/TAmeBlog/docs/Atcoder Begginer Contest/200s/276/abc278d_01',
+                component: ComponentCreator('/TAmeBlog/docs/Atcoder Begginer Contest/200s/276/abc278d_01', '43e'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/TAmeBlog/docs/Atcoder Begginer Contest/200s/278/abc278d_01',
+                component: ComponentCreator('/TAmeBlog/docs/Atcoder Begginer Contest/200s/278/abc278d_01', 'ae2'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/TAmeBlog/docs/Atcoder Begginer Contest/200s/283/abc283d_01',
+                component: ComponentCreator('/TAmeBlog/docs/Atcoder Begginer Contest/200s/283/abc283d_01', '7bf'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/TAmeBlog/docs/Atcoder Begginer Contest/200s/288/abc288c_01',
+                component: ComponentCreator('/TAmeBlog/docs/Atcoder Begginer Contest/200s/288/abc288c_01', '94c'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/TAmeBlog/docs/Atcoder Begginer Contest/200s/297/abc297d_01',
+                component: ComponentCreator('/TAmeBlog/docs/Atcoder Begginer Contest/200s/297/abc297d_01', '48d'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/TAmeBlog/docs/Atcoder Begginer Contest/300s/318/abc318d_01',
+                component: ComponentCreator('/TAmeBlog/docs/Atcoder Begginer Contest/300s/318/abc318d_01', 'e61'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/TAmeBlog/docs/Atcoder Begginer Contest/300s/321/abc321e_01',
+                component: ComponentCreator('/TAmeBlog/docs/Atcoder Begginer Contest/300s/321/abc321e_01', '53d'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/TAmeBlog/docs/Atcoder Begginer Contest/300s/322/abc322d_01',
+                component: ComponentCreator('/TAmeBlog/docs/Atcoder Begginer Contest/300s/322/abc322d_01', 'e1c'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/TAmeBlog/docs/Atcoder Begginer Contest/300s/331/abc331c_01',
+                component: ComponentCreator('/TAmeBlog/docs/Atcoder Begginer Contest/300s/331/abc331c_01', 'dac'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/TAmeBlog/docs/Atcoder Begginer Contest/300s/333/abc333d_01',
+                component: ComponentCreator('/TAmeBlog/docs/Atcoder Begginer Contest/300s/333/abc333d_01', 'b77'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/TAmeBlog/docs/Atcoder Begginer Contest/300s/336/abc336c_01',
+                component: ComponentCreator('/TAmeBlog/docs/Atcoder Begginer Contest/300s/336/abc336c_01', '511'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/TAmeBlog/docs/Atcoder Begginer Contest/300s/337/abc337d_01',
+                component: ComponentCreator('/TAmeBlog/docs/Atcoder Begginer Contest/300s/337/abc337d_01', 'c26'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/TAmeBlog/docs/Atcoder Begginer Contest/300s/353/abc353d_01',
+                component: ComponentCreator('/TAmeBlog/docs/Atcoder Begginer Contest/300s/353/abc353d_01', 'e83'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/TAmeBlog/docs/intro',
+                component: ComponentCreator('/TAmeBlog/docs/intro', 'd1f'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    path: '/TAmeBlog/',
+    component: ComponentCreator('/TAmeBlog/', '519'),
+    exact: true
+  },
+  {
+    path: '*',
+    component: ComponentCreator('*'),
+  },
+];
